cdb01b572370446414183c9b7535bc8fee4ea067
Pavlo <paulmanuilenko@gmail.com>
2020-05-15 17:47:37 +0300

    Add solution for module 24

---
+++ EPAM_RD_Auto_Java/Module_24_Task_1_base_source/jdbc/src/main/java/com/epam/rd/module24/databases/App.java
@@ -0,0 +1,13 @@
+package com.epam.rd.module24.databases;
+
+
+/**
+ * Class illustrates the CRUD operations with tables clients and accounts.
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ EPAM_RD_Auto_Java/Module_24_Task_1_base_source/jdbc/src/main/java/com/epam/rd/module24/databases/interfaces/CRUD.java
@@ -0,0 +1,8 @@
+package com.epam.rd.module24.databases.interfaces;
+
+/**
+ * The interface describes methods for implementing CREATE, READ, UPDATE, DELETE operations on database tables.
+ */
+public interface CRUD<T> {
+
+}
